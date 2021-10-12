5166fe288ae954137a67180a9a4087f7f36ba83b
Lars Holen <larsholen6@hotmail.com>
2021-10-12 14:27:39 +0200

    Add project files.

---
+++ DiabloTestProject/DiabloUnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace DiabloTestProject
+{
+    public class DiabloUnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
---
+++ NoroffAssignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoroffAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
