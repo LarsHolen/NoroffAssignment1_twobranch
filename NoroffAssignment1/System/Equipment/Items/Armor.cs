08fd6fb3204ca595702a967ce13f2d913cfda6cc
Lars Holen <larsholen6@hotmail.com>
2021-10-26 13:13:15 +0200

    Duh. Test first, then commit..

--- NoroffAssignment1/System/Equipment/Items/Armor.cs
+++ NoroffAssignment1/System/Equipment/Items/Armor.cs
@@ -4,7 +4,7 @@ using NoroffAssignment1.System.Enums;
 
 namespace NoroffAssignment1.System.Equipment.Items
 {
-    public class Armor : Item, IItem
+    public class Armor : Item
     {
         public ArmorType ArmorType { get; set; }
     }
